b54bb8a28213a9195f7ccad453725db66e4668b0
Tanish Chopra <tanish.18287@sakec.ac.in>
2026-05-18 18:45:14 +0530

    Refine1

--- eventmanager/urls.py
+++ eventmanager/urls.py
@@ -1,25 +1,14 @@
-"""
-URL configuration for eventmanager project.
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-
-from django.urls import path, include
+app_name = 'events'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('events.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.event_list, name='event_list'),
+    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('event/<int:event_id>/schedule/new/', views.schedule_create, name='schedule_create'),
+    path('schedule/<int:schedule_id>/update/', views.schedule_update, name='schedule_update'),
+    path('schedule/<int:schedule_id>/delete/', views.schedule_delete, name='schedule_delete'),
+    path('register/', views.register, name='register'),
+    path('slot/<int:slot_id>/register/', views.slot_register, name='slot_register'),
 ]
